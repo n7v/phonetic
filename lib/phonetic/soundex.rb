c7e4dac148ceaed653f7e778cd5358e9d4e3709e
Sem <novsem@gmail.com>
2013-10-19 23:36:52 +0300

    doc: add authors of Soundex

--- lib/phonetic/soundex.rb
+++ lib/phonetic/soundex.rb
@@ -1,7 +1,8 @@
 require 'phonetic/algorithm'
 
 module Phonetic
-  # Implementation of Soundex phonetic algorithm.
+  # Implementation of Soundex phonetic algorithm, which was developed by Robert C. Russell and Margaret K. Odell.
+  #
   # @example
   #    Phonetic::Soundex.encode('Ackerman') # => 'A265'
   #    Phonetic::Soundex.encode('ammonium') # => 'A500'
