71fadb98311c9008a0f1da43bca1c7b7a203058f
novsem <novsem@gmail.com>
2013-10-21 16:43:38 +0300

    doc: add description and examples for string extension for Refinded Soundex

--- lib/phonetic/core_ext/string/refined_soundex.rb
+++ lib/phonetic/core_ext/string/refined_soundex.rb
@@ -1,6 +1,11 @@
 require 'phonetic/refined_soundex'
 
 class String
+  # Refined Soundex value of string.
+  # @example
+  #    'Caren'.refined_soundex   # => 'C30908'
+  #    'Hayers'.refined_soundex  # => 'H093'
+  #    'Lambard'.refined_soundex # => 'L7081096'
   def refined_soundex(options = { trim: true })
     Phonetic::RefinedSoundex.encode(self, options)
   end
